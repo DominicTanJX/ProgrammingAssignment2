3828919e65552bde2159efd0350085d504cd7343
Dominic Tan <domtanjx@hotmail.com>
2015-02-21 18:29:32 +0800

    updated cachematrix.R with the required solutions and inclusions of comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,23 @@
 ## Assignment: Caching the Inverse of a Matrix
 
 ## This function creates a special "matrix" object that can cache its inverse.
+## This function creates a list that contains four member functions: set, get, 
+## setinverse and getinverse. The <<- assignment operator is used to assign a 
+## value to an object in an environment that is different from the current 
+## environment.
 
 makeCacheMatrix <- function(x = matrix()) {
        
         m <- NULL
         set <- function(y) {
-          x <<- y
-          m <<- NULL
+          x <<- y     ## sets the input matrix into the x matrix variable 
+          m <<- NULL  
         }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        
+        get <- function() x  ## returns the input matrix
+        setinverse <- function(inverse) m <<- inverse  ## sets the inversed matrix
+        getinverse <- function() m  ## returns the inversed matrix
+        
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse) 
@@ -27,14 +33,17 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-        m <- x$getinverse()
+        m <- x$getinverse()   ## gets the inversed matrix object of x to be assigned to m
+        
+        ## checks if the m variable is cached by fulfilling the is not null statement before executing the following
         if(!is.null(m)) {
           message("getting cached data")
           return(m)
         }
-        data <- x$get()
-        m <- solve(data, ...)
+        
+        data <- x$get()   ## gets the get function of the x object to be assigned to data variable
+        m <- solve(data, ...)  ## solves the data by inversing the values in data variable and assigning it to the m variable
         x$setinverse(m)
-        m
+        m  ## returns the value of the m variable
         
 }
